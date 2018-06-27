b769d1b5e9e573d621aa26723f247c6175116c82
Tobias Bocanegra <tripodsan@users.noreply.github.com>
2018-06-27 11:54:56 +0900

    use correct license header (fixes #200) (#209)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 /*
- *  Copyright 2018 Adobe Systems Incorporated. All rights reserved.
- *  This file is licensed to you under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License. You may obtain a copy
- *  of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under
- *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- *  OF ANY KIND, either express or implied. See the License for the specific language
- *  governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 
 module.exports = {
   'env': {
-    'node': true
+    'node': true,
+    'es6': true
   },
   // this is the root project for all sub modules. stop searching for any
   // eslintrc files in parent directories.
@@ -22,8 +23,28 @@ module.exports = {
     // async/await support
     'ecmaVersion': 8
   },
+  'plugins': [
+    'header',
+  ],
   'extends': 'airbnb',
   'rules': {
-    'strict': 0
-   }
+    'strict': 0,
+
+    // allow dangling underscores for 'fields'
+    'no-underscore-dangle': ['error', {'allowAfterThis': true}],
+
+    // enforce license header (todo: improve plugin to support patterns for multi-lines)
+    'header/header': [2, 'block', ['',
+      ' * Copyright 2018 Adobe. All rights reserved.',
+      ' * This file is licensed to you under the Apache License, Version 2.0 (the "License");',
+      ' * you may not use this file except in compliance with the License. You may obtain a copy',
+      ' * of the License at http://www.apache.org/licenses/LICENSE-2.0',
+      ' *',
+      ' * Unless required by applicable law or agreed to in writing, software distributed under',
+      ' * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS',
+      ' * OF ANY KIND, either express or implied. See the License for the specific language',
+      ' * governing permissions and limitations under the License.',
+      ' ',
+    ]]
+  }
 };
