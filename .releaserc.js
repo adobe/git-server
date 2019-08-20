6da55d24aa548d2ab466c30ca8be4cd4f969138d
Tobias Bocanegra <tripod@adobe.com>
2019-08-20 09:33:37 -0700

    chore(release): use semantic release

---
+++ .releaserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+    }],
+    "@semantic-release/npm",
+    ['@semantic-release/git', {
+      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }],
+    ['@semantic-release/exec', {
+      publishCmd: 'npm run deploy'
+    }],
+    '@semantic-release/github',
+  ],
+  branch: 'master'
+};
